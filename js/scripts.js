4ef5f8988436ae2156c7c4e4421ceffdbae256a4
Maarckz <48658125+Maarckz@users.noreply.github.com>
2025-05-10 08:56:17 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -32,5 +32,5 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
-document.addEventListener("contextmenu", event => event.preventDefault());
-document.addEventListener("selectstart", event => event.preventDefault());
+//document.addEventListener("contextmenu", event => event.preventDefault());
+//document.addEventListener("selectstart", event => event.preventDefault());
